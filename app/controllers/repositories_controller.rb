bc0db5f06ab47c37eb22ae5e24af4270b2837f19
Vishal Sharma <vsh1819@gmail.com>
2018-08-17 04:27:25 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,5 +5,27 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
+
+
+    begin
+      @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
+          req.params['client_id'] = 2083946415a606cfb59b
+          req.params['client_secret'] = 827b73b2c32fd31149eec8cb07196e0b1555ea5d
+          req.params['v'] = '20160201'
+          req.params['near'] = params[:zipcode]
+          req.params['query'] = 'coffee shop'
+          req.options.timeout = 0
+        end
+        body = JSON.parse(@resp.body)
+        if @resp.success?
+          @venues = body["response"]["venues"]
+        else
+          @error = body["meta"]["errorDetail"]
+        end
+
+      rescue Faraday::ConnectionFailed
+        @error = "There was a timeout. Please try again."
+      end
+      render 'search'
   end
 end
