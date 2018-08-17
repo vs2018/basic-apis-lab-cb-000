b8fe95700ed1cc6712e256a5c0d40a58a88bf399
Vishal Sharma <vsh1819@gmail.com>
2018-08-17 04:28:09 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -11,10 +11,8 @@ class RepositoriesController < ApplicationController
       @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
           req.params['client_id'] = 2083946415a606cfb59b
           req.params['client_secret'] = 827b73b2c32fd31149eec8cb07196e0b1555ea5d
-          req.params['v'] = '20160201'
-          req.params['near'] = params[:zipcode]
-          req.params['query'] = 'coffee shop'
-          req.options.timeout = 0
+          req.params['q'] = params[:q]
+      
         end
         body = JSON.parse(@resp.body)
         if @resp.success?
