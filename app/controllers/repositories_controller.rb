0ca3b146aef2f228875315dff284ea7c35cf6f3d
Vishal Sharma <vsh1819@gmail.com>
2018-08-17 04:32:02 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -23,9 +23,9 @@ class RepositoriesController < ApplicationController
         #   @error = body["meta"]["errorDetail"]
         # end
 
-      rescue Faraday::ConnectionFailed
-        @error = "There was a timeout. Please try again."
-      end
+      # rescue Faraday::ConnectionFailed
+      #   @error = "There was a timeout. Please try again."
+      # end
       render 'search'
   end
 end
