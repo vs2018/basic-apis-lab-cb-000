d6d5114eb378df3022307e3929e2ab766e7bfdcd
Vishal Sharma <vsh1819@gmail.com>
2018-08-17 04:31:01 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -15,13 +15,13 @@ class RepositoriesController < ApplicationController
 
         end
         body = JSON.parse(@resp.body)
-        if @resp.success?
+        # if @resp.success?
           @name = body["items"][0]["name"]
           @url = body["items"][1]["name"]
           @description = body["items"][1]["description"]
-        else
-          @error = body["meta"]["errorDetail"]
-        end
+        # else
+        #   @error = body["meta"]["errorDetail"]
+        # end
 
       rescue Faraday::ConnectionFailed
         @error = "There was a timeout. Please try again."
