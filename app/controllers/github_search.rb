b5818403a2dacf1b3d4f0fc8dee2396fb16f7a24
Vishal Sharma <vsh1819@gmail.com>
2018-08-17 04:26:33 +0000

    automatically backed up by learn

--- app/controllers/github_search.rb
+++ app/controllers/github_search.rb
@@ -1,7 +1,7 @@
 begin
   @resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
-      req.params['client_id'] = client_id
-      req.params['client_secret'] = client_secret
+      req.params['client_id'] = 2083946415a606cfb59b
+      req.params['client_secret'] = 827b73b2c32fd31149eec8cb07196e0b1555ea5d
       req.params['v'] = '20160201'
       req.params['near'] = params[:zipcode]
       req.params['query'] = 'coffee shop'
